7e99244cd7b6132ae88dbaba17c2c47f55b2b82f
safv12 <ing.fran12@gmail.com>
2016-02-25 08:51:38 -0700

    Add trello auth

---
+++ client/src/trello_auth.js
@@ -0,0 +1,12 @@
+Trello.authorize({
+  type: 'popup',
+  name: 'trello_metrics',
+  scope: { read: true, write: true },
+  expiration: 'never',
+  success: function() { console.log('Succesful authentication'); },
+  error: function() { console.log('Failed authentication'); }
+});
+
+Trello.get('/members/me').then(function(response) {
+  console.log(response);
+});
