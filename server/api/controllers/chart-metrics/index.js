dd50b27141fbe29444e02ef30ed4bd6106d0a779
safv12 <ing.fran12@gmail.com>
2016-04-18 07:38:30 -0500

    Update cumulative flow chart, add average

--- server/api/controllers/chart-metrics/index.js
+++ server/api/controllers/chart-metrics/index.js
@@ -3,13 +3,12 @@
 var utils = require('../utils');
 
 
-
 function createColumnsObj(lists) {
   var columns = {};
 
   for(var list in lists) {
     lists[list].forEach(function(column) {
-      columns[column.name] = 0;
+      columns[column.name] = { duration: 0, cards: 0 };
     });
   }
 
@@ -20,30 +19,30 @@ function createColumnsObj(lists) {
 
 exports.cumulativeFlow = function(cards, lists) {
   var columns = createColumnsObj(lists);
-  // TODO: Get Average
   cards.forEach(function(card) {
     var listName = utils.searchList(card.idList, lists);
 
     if (listName === 'done') {
       card.time.listTime.forEach(function(step) {
         var listStep = utils.searchList(step.list.id, lists);
-        if (listStep !== 'done') {
-          columns[step.list.name] += step.duration;
+        if (listStep !== 'done' && columns[step.list.name]) {
+          columns[step.list.name].duration += step.duration;
+          columns[step.list.name].cards ++;
         }
       });
     }
-
   });
 
   var cumulativeFlow = [];
   for (var column in columns) {
     var current = {};
-    var duration = utils.getHumanReadableTime(columns[column]);
+    var average = columns[column].duration / columns[column].cards;
+    var duration = utils.getHumanReadableTime(average);
 
     if (columns[column]) {
       current = {
         name: column,
-        data: [duration.time],
+        data: [ duration.time ],
         time: duration.format
       };
 
