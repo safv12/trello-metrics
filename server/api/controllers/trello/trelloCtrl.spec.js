70ac4865e1f2acfceddcbf505a1e39c84ae527fe
safv12 <ing.fran12@gmail.com>
2016-05-16 20:38:49 -0500

    Add test for get trello lists

--- server/api/controllers/trello/trelloCtrl.spec.js
+++ server/api/controllers/trello/trelloCtrl.spec.js
@@ -4,9 +4,22 @@ var should = require('should');
 describe('Trello controller tests', function() {
   'use strict';
   this.timeout(15000);
+  var boardId;
+
 
   it('should get all trello boards', function(done) {
     request.post('/v1/trello/me/boards')
+    .expect(200, function(err, res) {
+      if (err) { return done(err); }
+      should(res.body.length).be.above(0);
+      boardId = res.body[0].id;
+      done();
+    });
+  });
+
+
+  it('should get a board\'s lists', function(done) {
+    request.get('/v1/trello/boards/' + boardId + '/lists')
     .expect(200, function(err, res) {
       if (err) { return done(err); }
       should(res.body.length).be.above(0);
