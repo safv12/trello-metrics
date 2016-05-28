f73ceb37072ed60203b13bc900ed4a801377e5bb
safv12 <ing.fran12@gmail.com>
2016-05-27 21:27:31 -0600

    Refactor get options git status

--- server/api/controllers/cumulative-flow/cumulativeFlowCtrl.js
+++ server/api/controllers/cumulative-flow/cumulativeFlowCtrl.js
@@ -3,45 +3,54 @@
 var utils = require('../utils/utilsCtrl.js');
 
 
-function createColumnsObj(lists) {
-  var columns = {};
+/**
+ * Create options to highcharts graphs
+ * @param  {json}   lists of trello board
+ * @return {json}   json with highcharts format options
+ */
+function createChartOptions(lists) {
+  var bars = {};
 
   for(var list in lists) {
     lists[list].forEach(function(column) {
-      columns[column.name] = { duration: 0, cards: 0 };
+      bars[column.name] = {
+        duration: 0,
+        cards: 0
+      };
     });
   }
 
-  return columns;
+  return bars;
 }
 
 
 
 exports.cumulativeFlow = function(cards, lists) {
-  var columns = createColumnsObj(lists);
+  var charOptions = createChartOptions(lists);
+
   cards.forEach(function(card) {
     var listName = utils.searchList(card.idList, lists);
 
     if (listName === 'done') {
       card.time.listTime.forEach(function(step) {
         var listStep = utils.searchList(step.list.id, lists);
-        if (listStep !== 'done' && columns[step.list.name]) {
-          columns[step.list.name].duration += step.duration;
-          columns[step.list.name].cards ++;
+        if (listStep !== 'done' && charOptions[step.list.name]) {
+          charOptions[step.list.name].duration += step.duration;
+          charOptions[step.list.name].cards ++;
         }
       });
     }
   });
 
   var cumulativeFlow = [];
-  for (var column in columns) {
+  for (var bar in charOptions) {
     var current = {};
-    var average = columns[column].duration / columns[column].cards;
+    var average = charOptions[bar].duration / charOptions[bar].cards;
     var duration = utils.getHumanReadableTime(average);
 
-    if (columns[column]) {
+    if (charOptions[bar]) {
       current = {
-        name: column,
+        name: bar,
         data: [ duration.time ],
         time: duration.format
       };
